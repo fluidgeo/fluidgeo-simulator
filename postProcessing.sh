ddf66c58011bc5ac349b07e4ab0bb802b068084f
volpatto <volpatto@lncc.br>
2015-09-12 18:05:54 -0300

    I forgot to add the shell of post-processing in the last push. So here I fix it.

---
+++ postProcessing.sh
@@ -0,0 +1,38 @@
+#set -x
+#  Script to automatize post-processing of output processing program.
+#  
+#  Author: Diego Volpatto
+#  
+#  Usage example: ./postProcessing.sh dirExp
+#  dirExp is the directory that data will be read in post-processing programs.
+
+dirExp=$1
+
+comando="chmod u+x ./fontes/plot_P.py"
+eval ${comando}
+
+comando="chmod u+x ./fontes/plotDisp.py"
+eval ${comando}
+
+comando="./fontes/plot_P.py"
+eval ${comando}
+
+comando="./fontes/plotDisp.py"
+eval ${comando}
+
+#comando="./montarPDFGraficos.sh ${dirExp}"
+#eval ${comando} > saida 
+
+rm -f ${dirExp}/disp.*
+rm -f ${dirExp}/solP.*
+rm -f ${dirExp}/gradPx.*
+rm -f ${dirExp}/gradPy.*
+rm -f ${dirExp}/solVelocity_x.*
+rm -f ${dirExp}/solVelocity_y.*
+rm -f ${dirExp}/nodeFlux_x.*
+rm -f ${dirExp}/nodeFlux_y.*
+rm -f ${dirExp}/residueFlux_x.*
+# rm ${dirExp}/residueFlux_y.*
+
+#rm -f saida
+
